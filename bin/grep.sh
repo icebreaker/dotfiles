830f6a1ddb75e4e5b0a6a64d3d8186fe1dbff2df
Mihail Szabolcs <theicebreaker007@gmail.com>
2017-01-01 04:16:48 +0200

    added custom grep for vim

---
+++ bin/grep.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DIR=`pwd`
+GREP='.grep'
+
+while test "${DIR}" != "/"; do
+	if [ -f "${DIR}/${GREP}" ]; then
+		ARGS=$(cat "${DIR}/${GREP}" | tr '\n' ' ')
+		grep -n -R ${ARGS} ${@} "${DIR}"
+		exit 0
+	fi
+
+	DIR=`dirname "${DIR}"`
+done
+
+grep -n -R ${@}
