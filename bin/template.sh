918d53dc135fc31792f5a1c062edf622bea2033a
Mihail Szabolcs <theicebreaker007@gmail.com>
2018-02-12 22:25:06 +0200

    added filepath to templater

--- bin/template.sh
+++ bin/template.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
-DIR=`pwd`
+DIR=$(pwd)
 TEMPLATE='.template'
 
 function process_template()
 {
 	eval "echo \"$(cat $1)\""
+}
+
+function extract_filepath()
+{
+	local FILEPATH=""
+	local SRCFOUND=0
+
+	for s in $(dirname $1 | tr '/' '\n'); do
+		if [ $SRCFOUND -eq 1 ]; then
+			if [ -z "$FILEPATH" ]; then
+				FILEPATH=$s
+			else
+				FILEPATH="$FILEPATH/$s"
+			fi
+		elif [ "$s" == "src" ]; then
+			SRCFOUND=1
+		fi
+	done
+
+	echo $FILEPATH
 }
 
 if [ -n "$1" ]; then
@@ -15,15 +35,17 @@ if [ -n "$1" ]; then
 	FILENAME=$(basename "$1" ".${EXTENSION}")
 	FILENAME_UP=$(echo "$FILENAME" | tr '[a-z]' '[A-Z]')
 
+	FILEPATH=$(extract_filepath "$1")
+
 	if [ -n "$EXTENSION" ]; then
 		TEMPLATE=".template.${EXTENSION}"
 	fi
 fi
 
-while test "${DIR}" != "/"; do
-	if [ -f "${DIR}/${TEMPLATE}" ]; then
-		process_template "${DIR}/${TEMPLATE}"
+while [ "$DIR" != "/" ]; do
+	if [ -f "$DIR/$TEMPLATE" ]; then
+		process_template "$DIR/$TEMPLATE"
 		exit 0
 	fi
-	DIR=`dirname "${DIR}"`
+	DIR=$(dirname "$DIR")
 done
