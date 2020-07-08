6ed1079115efcf564302288b07c844af8e393365
Mihail Szabolcs <theicebreaker007@gmail.com>
2020-07-08 23:52:16 +0300

    adjusted template folding

--- vim/vim.symlink/templates/template.cc
+++ vim/vim.symlink/templates/template.cc
@@ -1,4 +1,4 @@
-/* {{{
+/*{{{
 	MIT LICENSE
 
 	Copyright (c) %YEAR%, Mihail Szabolcs
@@ -20,7 +20,7 @@
 	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 	SOFTWARE.
-}}} */
+}}}*/
 #include "%INCLUDEPATH%%FILENAME%.h"
 
 /* vim: set ts=4 sw=4 sts=4 noet: */
--- vim/vim.symlink/templates/template.lua
+++ vim/vim.symlink/templates/template.lua
@@ -1,4 +1,4 @@
---[[ {{{
+--[[{{{
 	MIT LICENSE
 
 	Copyright (c) %YEAR%, Mihail Szabolcs
@@ -20,6 +20,6 @@
 	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 	SOFTWARE.
-}}} --]]
+}}}--]]
 
 -- vim: set ts=2 sw=2 sts=2 noet:
