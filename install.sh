ba7efaad095d97102f2e7dc2a2f19d8adeda5c86
Mihail Szabolcs <theicebreaker007@gmail.com>
2020-09-01 17:51:09 +0300

    defaulted to default codespaces install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$HOME" == "/home/codespace" ]; then
+	mv ~/.bashrc ~/.codespacesrc
+	mv ~/dotfiles ~/.dotfiles
+	~/.dotfiles/bin/dot-install
+else
+	echo "This install script is for codespaces only! Please run bin/dot-install"
+fi
