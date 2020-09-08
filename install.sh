1b9e67244814424d670a88076016dd25d48bb9c0
Mihail Szabolcs <theicebreaker007@gmail.com>
2020-09-08 13:36:58 +0300

    source bashrc after codespaces install

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ if [ "$HOME" == "/home/codespace" ]; then
 	mv ~/.bashrc ~/.codespacesrc
 	mv ~/dotfiles ~/.dotfiles
 	~/.dotfiles/bin/dot-install
+	source ~/.bashrc
 else
 	echo "This install script is for codespaces only! Please run bin/dot-install"
 fi
